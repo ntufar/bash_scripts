05b82ca0de0464669046456b8da828edd24fcea1
Nicolai Tufar <ntufar@gmail.com>
2013-08-22 23:08:51 +0400

    Some useful commands and Youtube video link

--- enable_public_html_in_se_linux.sh
+++ enable_public_html_in_se_linux.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 # from here: http://diegobz.net/2010/03/07/enabling-apache-userdir-public_html-with-selinux-enabled-on-fedora/
 
+# Linux SE Commands
+# ls -Z
+# id -Z
+# ps -Z
+# netstat -Z
+
+# sealert
+# chcon, resorecon
+#  
+# getsebool -a
+#
+# restorecon
+# audit2allow
+#
+# system-config-selinux
+#
+# Video: http://www.youtube.com/watch?v=MxjenQ31b70
+
+
 setsebool -P httpd_enable_homedirs true
-chcon -R -t httpd_sys_content_t ~<username>/public_html  #Might not be necessary
+#chcon -R -t httpd_sys_content_t ~<username>/public_html  #Might not be necessary
