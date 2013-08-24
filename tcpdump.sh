9c751732cc7a17bdcff6445a603682af8cb02b4d
Nicolai Tufar <ntufar@gmail.com>
2013-08-25 02:47:18 +0400

    RIP requests

--- tcpdump.sh
+++ tcpdump.sh
@@ -6,3 +6,6 @@ tcpdump -i wlp2s0 arp
 
 # ignore domain, http, hrsp, and https ports
 tcpdump -i wlp2s0 -n ip and not port domain and not port http and not port https and port not hsrp and port not xmpp-client and not arp
+
+# Dump RIP requests
+tcpdump -i wlp2s0 -n -v -s0 udp and port 520
