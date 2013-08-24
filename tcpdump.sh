2b36343043f653eb6671aa3547d7d696fb9a0d1c
Nicolai Tufar <ntufar@gmail.com>
2013-08-24 20:23:07 +0400

    No need for arp too

--- tcpdump.sh
+++ tcpdump.sh
@@ -5,4 +5,4 @@
 tcpdump -i wlp2s0 arp
 
 # ignore domain, http, hrsp, and https ports
-tcpdump -i wlp2s0 -n ip and not port domain and not port http and not port https and port not hsrp and port not xmpp-client
+tcpdump -i wlp2s0 -n ip and not port domain and not port http and not port https and port not hsrp and port not xmpp-client and not arp
